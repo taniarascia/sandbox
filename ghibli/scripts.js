1f6d2ad30622ce2bfadad0b1d32d5853e756aea6
Scott Geoffrey <gcs1usa@aol.com>
2019-09-19 04:34:19 -0400

    JS Experiment
    Changing "Movie" to "Flick"

--- ghibli/scripts.js
+++ ghibli/scripts.js
@@ -16,16 +16,16 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(flick => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = flick.title;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      flick.description = flick.description.substring(0, 300);
+      p.textContent = `${flick.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
