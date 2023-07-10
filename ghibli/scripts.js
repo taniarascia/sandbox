14dd744749f71d9da5c047f12aae18bddca02617
Dominik K <mezo.dsn@gmail.com>
2023-07-10 17:40:57 +0200

    fix: ghibli example
    This makes the ghibli example work again with a new API with a updated and improved dataset!

--- ghibli/scripts.js
+++ ghibli/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://ghibli.rest/films', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
